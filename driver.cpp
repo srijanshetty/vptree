502e8b4aaca8825a19c64f8af750a702fdcf4617
Srijan R Shetty <srijan.shetty@gmail.com>
2015-03-15 21:55:11 +0530

    buildTree

--- driver.cpp
+++ driver.cpp
@@ -25,18 +25,72 @@
 
 // Stream processing
 #include <iostream>
+#include <fstream>
+
+// Limits of numbers
+#include <limits>
 
 using namespace std;
 using namespace VPTree;
 
+// Build the tree from reading the DATAFILE
+void buildTree(Node *root) {
+    ifstream ifile;
+    ifile.open(DATAFILE, ios::in);
+
+    // Items to read from the file
+    vector <double> point;
+    double coordinate;
+    string dataString;
+    long long objectID;
+
+    while (!ifile.eof()) {
+        // Get the point
+        point.clear();
+        for (long i = 0; i < DIMENSIONS; ++i) {
+            ifile >> coordinate;
+            point.push_back(coordinate);
+        }
+
+        // Get the objectID and the dataString
+        ifile >> objectID >> dataString;
+
+        // Some quirk which needs to be handled
+        if (ifile.eof()) {
+            break;
+        }
+
+        if (objectID % 100 == 0) {
+            cout << endl << "Inserting " << objectID << " ";
+        }
+
+        // Insert the object into file
+        root->insert(DBObject(objectID, dataString, point), 0.0);
+    }
+
+    // Close the file
+    ifile.close();
+}
+
 int main() {
+    // Test points
     vector<double> p1 = {1.0, 0.0};
     vector<double> p2 = {4.0, 0.0};
 
     Point alpha(p1);
     Point beta(p2);
 
-    cout << alpha.distance(beta);
+    // cout << alpha.distance(beta);
+
+    // test objects
+    DBObject obj(0, "srijan", p1);
+
+    // Test the insertion routine
+    Node *VPRoot = new Node(numeric_limits<double>::max(), obj);
+    buildTree(VPRoot);
+
+    // Print tree
+    VPRoot->print();
 
     return 0;
 }
